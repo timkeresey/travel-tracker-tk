200fbe9d8e7f39094b6f013200ce2f551ce513f8
Tim Keresey <tim.keresey@gmail.com>
2020-09-19 15:58:47 -0600

    refactor class organization

---
+++ src/Traveler.js
@@ -0,0 +1,15 @@
+class Traveler {
+  constructor(traveler) {
+    this.id = traveler.id;
+    this.name = traveler.name;
+    this.pastTrips = 
+    this.currentTrip
+    this.upcomingTrips
+    this.pendingTrips
+  }
+
+}
+
+
+
+export default Traveler;
---
+++ src/Trip.js
@@ -0,0 +1,17 @@
+class Trip {
+  constructor(tripsData) {
+    this.id = tripsData.id;
+    this.userID = tripsData.userID;
+    this.destinationID = tripsData.destinationID;
+    this.travelers = tripsData.travelers;
+    this.date = tripsData.date;
+    this.duration = tripsData.duration;
+    this.status = tripsData.status;
+    this.suggestedActivities = [];
+  }
+
+}
+
+
+
+export default Trip;
