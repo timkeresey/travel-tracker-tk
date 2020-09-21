8a93e060a9fd5975c001d291a1e5967aeed3a3ee
Tim Keresey <tim.keresey@gmail.com>
2020-09-21 13:10:37 -0600

    import data to index.js

--- src/fetchHandler.js
+++ src/fetchHandler.js
@@ -1,29 +1,31 @@
 let fetchHandler = {
   fetchSingleTraveler() {
-    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1')// placeholder ID
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1')
     .then(response => response.json())
-    .then(response => response)
-    .catch(error => console.log('error' error))
+    .then(data => data)
+    .catch(error => console.log('error', error))
   },
 
   fetchTravelersData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
     .then(response => response.json())
-    .then(response => response.travelers)
-    .catch(error => console.log('error' error))
+    .then(data => data.travelers)
+    .catch(error => console.log('error', error))
   },
 
   fetchTripsData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
     .then(response => response.json())
-    .then(response => response.trips)
-    .catch(error => console.log('error' error))
+    .then(data => data.trips)
+    .catch(error => console.log('error', error))
   },
 
   fetchDestinationsData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
     .then(response => response.json())
-    .then(response => response.destinations)
-    .catch(error => console.log('error' error))
+    .then(data => data.destinations)
+    .catch(error => console.log('error', error))
   }
 }
+
+export default fetchHandler;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,27 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
+import moment from 'moment';
+import fetchHandler from './fetchHandler.js';
+import Traveler from './Traveler.js';
+import Trip from './Trip.js';
+import Destination from './Destination.js';
+import domUpdates from './domUpdates.js';
+// console.log('This is the JavaScript entry file - your code begins here.');
+window.addEventListener('load', getData())
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
+function getData() {
+  let userData = fetchHandler.fetchSingleTraveler();
+  let travelersData = fetchHandler.fetchTravelersData();
+  let tripsData = fetchHandler.fetchTripsData();
+  let destinationsData = fetchHandler.fetchDestinationsData();
+  Promise.all([
+    userData,
+    travelersData,
+    tripsData,
+    destinationsData
+  ])
+  .then(data => {
+    console.log('all this stuff', data);
+  })
+}
